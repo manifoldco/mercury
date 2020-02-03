ed5ff27c8811dac98a004314e58a1b884ced8266
Drew Powers <drew@pow.rs>
2020-02-03 10:14:55 -0700

    Add Figma assets

---
+++ example-codebases/web/webpack.config.js
@@ -0,0 +1,44 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const DiezWebpackPlugin = require('diez-webpack-plugin');
+
+const webpackConfig = {
+  entry: './src/index.jsx',
+  resolve: {symlinks: false},
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {modules: true}},
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CopyWebpackPlugin([
+      {from: 'public'},
+      {from: 'public/diez', to: 'diez'},
+    ]),
+    new DiezWebpackPlugin({
+      sdk: 'diez-mercury',
+    }),
+  ],
+  performance: {hints: false},
+  devServer: {stats: 'errors-only', open: true, hot: true},
+};
+
+module.exports = webpackConfig;
