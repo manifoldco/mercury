39e7d8443834b7ca368ec4a4f0dd3e43ee2f11b1
Drew Powers <drew@pow.rs>
2020-03-31 15:01:20 -0600

    Update Figma 1585688480

--- scripts/strip-icon-colors.js
+++ scripts/strip-icon-colors.js
@@ -7,9 +7,6 @@ const path = require('path');
 const ICON_DIR = path.resolve(__dirname, '..', 'src', 'icons');
 
 fs.readdirSync(ICON_DIR).forEach((icon) => {
-  const svg = fs
-    .readFileSync(path.resolve(ICON_DIR, icon), 'utf8')
-    .replace(/stroke="#[a-z0-9]+"\s*/gi, '') // remove hardcoded stroke colors (but leave "none")
-    .replace(/fill="#[a-z0-9]+"\s*/gi, ''); // remove hardcoded fill colors (but leave "none")
+  const svg = fs.readFileSync(path.resolve(ICON_DIR, icon), 'utf8').replace(/fill="[^"]+"\s*/g, ''); // remove hardcoded fill colors (but leave "none")
   fs.writeFileSync(path.resolve(ICON_DIR, icon), svg, 'utf8');
 });
