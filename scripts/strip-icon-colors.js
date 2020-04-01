26d36db83fa4d924e9941d6d7b857a254617a92d
Drew Powers <drew@pow.rs>
2020-04-01 08:18:07 -0600

    Fix Figma Update cron job

--- scripts/strip-icon-colors.js
+++ scripts/strip-icon-colors.js
@@ -1,5 +1,5 @@
 /**
- * Remove unnecessary stroke and fill from SVGs
+ * Strip SVGs of colors
  */
 const fs = require('fs');
 const path = require('path');
@@ -7,6 +7,6 @@ const path = require('path');
 const ICON_DIR = path.resolve(__dirname, '..', 'src', 'icons');
 
 fs.readdirSync(ICON_DIR).forEach((icon) => {
-  const svg = fs.readFileSync(path.resolve(ICON_DIR, icon), 'utf8').replace(/fill="[^"]+"\s*/g, ''); // remove hardcoded fill colors (but leave "none")
+  const svg = fs.readFileSync(path.resolve(ICON_DIR, icon), 'utf8').replace(/\s*fill="[^"]+"/g, ''); // remove hardcoded fill colors (but leave "none")
   fs.writeFileSync(path.resolve(ICON_DIR, icon), svg, 'utf8');
 });
