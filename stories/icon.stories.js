c52e2b70bf07f2dc0563c46367e1c768bb9717a9
Drew Powers <drew@pow.rs>
2020-04-09 10:22:56 -0600

    Add copyable HTML to icons

--- stories/icon.stories.js
+++ stories/icon.stories.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import prettier from 'prettier/standalone';
+import parserHTML from 'prettier/parser-html';
 import icons from './storybook/icons.json';
+import { Story, Code } from './storybook/blocks';
 import './icon.scss';
 
 export default { title: 'Icons' };
 
-export const allIcons = () => (
+export const gallery = () => (
   <div className="Icon__Grid">
-    {Object.entries(icons).map(([name, svg]) => (
-      <div className="Icon">
-        <div className="Icon__SVG" dangerouslySetInnerHTML={{ __html: svg }} />
-        <div className="Icon__Name">{name.replace(/\.svg$/, '')}</div>
-      </div>
-    ))}
+    {Object.entries(icons).map(([filename, svg]) => {
+      const name = filename.replace(/\.svg$/, '');
+      return (
+        <Story small>
+          <div className="Icon__SVG" dangerouslySetInnerHTML={{ __html: svg }} />
+          <div className="Icon__Name">{name}</div>
+          <Code
+            tabs={{
+              html: prettier.format(svg.replace(/^<svg/, `<svg aria-label="${name}" role="img"`), {
+                parser: 'html',
+                plugins: [parserHTML],
+              }),
+            }}
+          />
+        </Story>
+      );
+    })}
   </div>
 );
