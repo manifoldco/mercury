2b8f8ae5db95569c395cbd9d34b4ce7594d540c2
Nick Tassone <ntassone@gmail.com>
2020-04-15 15:00:35 -0400

    updated stylelint to account for 3 selectors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   syntax: 'scss',
   rules: {
     'at-rule-no-unknown': null,
-    'selector-max-specificity': '0,1,2',
+    'selector-max-specificity': '0,1,3',
     'no-descending-specificity': null,
   },
 };
